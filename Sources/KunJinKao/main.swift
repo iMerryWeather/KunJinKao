4c9febe8b3ae0422233dd8a83d13b9883ba679a3
Louis Shen <louiseshen@protonmail.com>
2022-01-04 18:39:06 +0800

    (1a2) add cmd argv

--- Sources/KunJinKao/main.swift
+++ Sources/KunJinKao/main.swift
@@ -11,6 +11,7 @@ func big5ToGBK(big5String : String) -> NSString {
     return NSString(data: big5Data, encoding: gbkEnc)!
 }
 
+let VERSION = "kunjinkao 1.0 (1A2)"
 let STARTUP_MSG =
 """
 Usage:    kunjinkao
@@ -18,11 +19,23 @@ Usage:    kunjinkao
   -v, --version  print the kunjinkao version.
 """
 
-var inputs = [String]()
-while let input = readLine() {
-    inputs.append(input)
-}
+//deal with command line arguments
+let argv = CommandLine.arguments
+if argv.count != 1 {
+    if argv[1] == "-h" || argv[1] == "--help" {
+        print(STARTUP_MSG)
+    } else if argv[1] == "-v" || argv[1] == "--version" {
+        print(VERSION)
+    } else {
+        print("kunjinkao: invalid option -- \(argv[1])")
+    }
+} else {
+    var inputs = [String]()
+    while let input = readLine() {
+        inputs.append(input)
+    }
 
-for i in inputs {
-    print(big5ToGBK(big5String: i))
+    for i in inputs {
+        print(big5ToGBK(big5String: i))
+    }
 }
